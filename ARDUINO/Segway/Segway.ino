acba1187e46b58616c21226680a409276a8753f5
ChronoVenom <billli1248@gmail.com>
2017-03-28 10:29:36 -0700

    Add files via upload

---
+++ ARDUINO/Segway/Segway.ino
@@ -0,0 +1,270 @@
+/* Copyright (C) 2012 Kristian Lauszus, TKJ Electronics. All rights reserved.
+ 
+ This software may be distributed and modified under the terms of the GNU
+ General Public License version 2 (GPL2) as published by the Free Software
+ Foundation and appearing in the file GPL2.TXT included in the packaging of
+ this file. Please note that GPL2 Section 2[b] requires that all works based
+ on this software must also be made publicly available under the terms of
+ the GPL2 ("Copyleft").
+ 
+ Contact information
+ -------------------
+ 
+ Kristian Lauszus, TKJ Electronics
+ Web      :  http://www.tkjelectronics.com
+ e-mail   :  kristianl@tkjelectronics.com
+ */
+
+#include <Wire.h>
+#include <FreeSixIMU.h>
+#include <FIMU_ADXL345.h>
+#include <FIMU_ITG3200.h>
+#include "Kalman.h" // Source: https://github.com/TKJElectronics/KalmanFilter
+
+#define dirA 12
+#define dirB 13
+#define speedA 3
+#define speedB 11
+#define brakeA 9
+#define brakeB 8
+#define currA A0
+#define currB A1
+
+#define KP 75
+#define KD 35 
+#define KI 00
+ 
+int16_t angle[2]; // pitch & roll
+ 
+// Set the FreeSixIMU object
+FreeSixIMU sixDOF = FreeSixIMU();
+float rawSixDof[6];
+Kalman kalmanX; // Create the Kalman instances
+Kalman kalmanY;
+
+Kalman kalman2X;
+Kalman kalman2Y;
+
+/* IMU Data */
+int16_t accX, accY, accZ;
+int16_t tempRaw;
+int16_t gyroX, gyroY, gyroZ;
+
+double accXangle, accYangle; // Angle calculate using the accelerometer
+double temp; // Temperature
+double gyroXangle, gyroYangle; // Angle calculate using the gyro
+double compAngleX, compAngleY; // Calculate the angle using a complementary filter
+double kalAngleX, kalAngleY; // Calculate the angle using a Kalman filter
+
+/* IMU Data 2*/
+int16_t acc2X, acc2Y, acc2Z;
+int16_t tempRaw2;
+int16_t gyro2X, gyro2Y, gyro2Z;
+
+double acc2Xangle, acc2Yangle; // Angle calculate using the accelerometer
+double temp2; // Temperature
+double gyro2Xangle, gyro2Yangle; // Angle calculate using the gyro
+double compAngle2X, compAngle2Y; // Calculate the angle using a complementary filter
+double kalAngle2X, kalAngle2Y; // Calculate the angle using a Kalman filter
+
+//PID controls
+double dAngle;
+double intAngle;
+double lastAngle;
+double Igx;
+double v_e;
+boolean fell_over;
+
+uint32_t timer;
+uint8_t i2cData[14]; // Buffer for I2C data
+
+void setup() {  
+  Serial.begin(115200);
+  pinMode(dirA, OUTPUT);
+  pinMode(dirB, OUTPUT);
+  pinMode(speedA, OUTPUT);
+  pinMode(speedB, OUTPUT);
+  pinMode(brakeA, OUTPUT);
+  pinMode(brakeB, OUTPUT);
+  digitalWrite(brakeA,LOW);
+  digitalWrite(brakeB,LOW);
+  fell_over = false;
+  
+  Wire.begin();
+  Wire.begin();
+  i2cData[0] = 7; // Set the sample rate to 1000Hz - 8kHz/(7+1) = 1000Hz
+  i2cData[1] = 0x00; // Disable FSYNC and set 260 Hz Acc filtering, 256 Hz Gyro filtering, 8 KHz sampling
+  i2cData[2] = 0x00; // Set Gyro Full Scale Range to ±250deg/s
+  i2cData[3] = 0x00; // Set Accelerometer Full Scale Range to ±2g
+  while(i2cWrite(0x19,i2cData,4,false)); // Write to all four registers at once
+  while((0x6B,0x01,true)); // PLL with X axis gyroscope reference and disable sleep mode 
+  
+  //Sensor Init
+  sixDOF.init();                        //begin the IMU
+  delay(5);
+
+  //PID controls
+  dAngle = 0;
+  intAngle = 0;
+  lastAngle = 0;
+  
+  while(i2cRead(0x75,i2cData,1));
+  if(i2cData[0] != 0x68) { // Read "WHO_AM_I" register
+    Serial.print(F("Error reading sensor"));
+    while(1);
+  }
+  
+  delay(100); // Wait for sensor to stabilize
+  
+  /* Set kalman and gyro starting angle */
+  sixDOF.getRawValues(rawSixDof);
+  while(i2cRead(0x3B,i2cData,6));
+  acc2X = rawSixDof[0];//((i2cData[0] << 8) | i2cData[1]);
+  acc2Y = rawSixDof[1];//((i2cData[2] << 8) | i2cData[3]);
+  acc2Z = rawSixDof[2];//((i2cData[4] << 8) | i2cData[5]);
+  
+  accX = ((i2cData[0] << 8) | i2cData[1]);
+  accY = ((i2cData[2] << 8) | i2cData[3]);
+  accZ = ((i2cData[4] << 8) | i2cData[5]);
+  
+  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
+  // We then convert it to 0 to 2π and then from radians to degrees
+  accYangle = (atan2(accX,accZ)+PI)*RAD_TO_DEG;
+  accXangle = (atan2(accY,accZ)+PI)*RAD_TO_DEG;
+  acc2Yangle = (atan2(acc2X,acc2Z)+PI)*RAD_TO_DEG;
+  acc2Xangle = (atan2(acc2Y,acc2Z)+PI)*RAD_TO_DEG;
+  
+  kalmanX.setAngle(accXangle); // Set starting angle
+  kalmanY.setAngle(accYangle);
+  gyroXangle = accXangle;
+  gyroYangle = accYangle;
+  compAngleX = accXangle;
+  compAngleY = accYangle;
+  
+  kalman2X.setAngle(acc2Xangle); // Set starting angle
+  kalman2Y.setAngle(acc2Yangle);
+  gyro2Xangle = acc2Xangle;
+  gyro2Yangle = acc2Yangle;
+  compAngle2X = acc2Xangle;
+  compAngle2Y = acc2Yangle;
+  
+  timer = micros();
+  Serial.println("done setup");
+}
+
+void loop() {
+  /* Update all the values */  
+  sixDOF.getRawValues(rawSixDof);
+  while(i2cRead(0x3B,i2cData,14));
+  accX = ((i2cData[0] << 8) | i2cData[1]);
+  accY = ((i2cData[2] << 8) | i2cData[3]);
+  accZ = ((i2cData[4] << 8) | i2cData[5]);
+  tempRaw = ((i2cData[6] << 8) | i2cData[7]);  
+  gyroX = ((i2cData[8] << 8) | i2cData[9]);
+  gyroY = ((i2cData[10] << 8) | i2cData[11]);
+  gyroZ = ((i2cData[12] << 8) | i2cData[13]);
+  
+  acc2X = rawSixDof[0];//((i2cData[0] << 8) | i2cData[1]);
+  acc2Y = rawSixDof[1];//((i2cData[2] << 8) | i2cData[3]);
+  acc2Z = rawSixDof[2];//((i2cData[4] << 8) | i2cData[5]);
+//  tempRaw = ((i2cData[6] << 8) | i2cData[7]);  
+  gyro2X = rawSixDof[3];//((i2cData[8] << 8) | i2cData[9]);
+  gyro2Y = rawSixDof[4];//((i2cData[10] << 8) | i2cData[11]);
+  gyro2Z = rawSixDof[5];//((i2cData[12] << 8) | i2cData[13]);
+  
+  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
+  // We then convert it to 0 to 2π and then from radians to degrees
+  accXangle = (atan2(accY,accZ)+PI)*RAD_TO_DEG;
+  accYangle = (atan2(accX,accZ)+PI)*RAD_TO_DEG;
+  acc2Xangle = (atan2(acc2Y,acc2Z)+PI)*RAD_TO_DEG;
+  acc2Yangle = (atan2(acc2X,acc2Z)+PI)*RAD_TO_DEG;
+  
+  double gyroXrate = (double)gyroX/131.0;
+  double gyroYrate = -((double)gyroY/131.0);
+  gyroXangle += gyroXrate*((double)(micros()-timer)/1000000); // Calculate gyro angle without any filter  
+  gyroYangle += gyroYrate*((double)(micros()-timer)/1000000);
+  //gyroXangle += kalmanX.getRate()*((double)(micros()-timer)/1000000); // Calculate gyro angle using the unbiased rate
+  //gyroYangle += kalmanY.getRate()*((double)(micros()-timer)/1000000);
+  
+  double gyro2Xrate = (double)gyro2X/131.0;
+  double gyro2Yrate = -((double)gyro2Y/131.0);
+  gyro2Xangle += gyro2Xrate*((double)(micros()-timer)/1000000); // Calculate gyro angle without any filter  
+  gyro2Yangle += gyro2Yrate*((double)(micros()-timer)/1000000);
+  
+  compAngleX = (0.93*(compAngleX+(gyroXrate*(double)(micros()-timer)/1000000)))+(0.07*accXangle); // Calculate the angle using a Complimentary filter
+  compAngleY = (0.93*(compAngleY+(gyroYrate*(double)(micros()-timer)/1000000)))+(0.07*accYangle);
+  
+  kalAngleX = kalmanX.getAngle(accXangle, gyroXrate, (double)(micros()-timer)/1000000); // Calculate the angle using a Kalman filter
+  kalAngleY = kalmanY.getAngle(accYangle, gyroYrate, (double)(micros()-timer)/1000000);
+
+//2
+  compAngle2X = (0.93*(compAngle2X+(gyro2Xrate*(double)(micros()-timer)/1000000)))+(0.07*acc2Xangle); // Calculate the angle using a Complimentary filter
+  compAngle2Y = (0.93*(compAngle2Y+(gyro2Yrate*(double)(micros()-timer)/1000000)))+(0.07*acc2Yangle);
+  
+  kalAngle2X = kalman2X.getAngle(acc2Xangle, gyro2Xrate, (double)(micros()-timer)/1000000); // Calculate the angle using a Kalman filter
+  kalAngle2Y = kalman2Y.getAngle(acc2Yangle, gyro2Yrate, (double)(micros()-timer)/1000000);  
+  
+  //temp = ((double)tempRaw + 12412.0) / 340.0;
+  
+  //fallen over?
+  if(abs(kalAngleX -180) > 45){
+    fell_over = true;
+  }
+  
+  //PID on kalAngleX
+  Igx = kalAngleX - 178;
+  dAngle = kalAngleX - lastAngle;
+  lastAngle = kalAngleX;
+  intAngle += Igx*(double)(micros() - timer)/1000000;
+  v_e = KP*Igx/**abs(Igx)*/ + KD*dAngle + KI*intAngle;
+  if(v_e > 0){
+    digitalWrite(dirA, LOW);
+    digitalWrite(dirB, HIGH); 
+  }
+  else{
+    digitalWrite(dirA, HIGH);
+    digitalWrite(dirB, LOW);    
+  }
+  if(fell_over){
+    analogWrite(speedA, 0);
+    analogWrite(speedB, 0);
+  }
+  else {
+    analogWrite(speedA, min(abs(v_e), 255));
+    analogWrite(speedB, min(abs(v_e), 255));
+  }
+  
+  timer = micros();
+  
+  /* Print Data */
+  /*
+  Serial.print(accX);Serial.print(",");
+  Serial.print(accY);Serial.print(",");
+  Serial.print(accZ);Serial.print(",");
+  
+  Serial.print(gyroX);Serial.print(",");
+  Serial.print(gyroY); Serial.print(",");
+  Serial.print(gyroZ);Serial.println("");
+  */
+  Serial.print(accXangle);Serial.print(",");
+  Serial.print(gyroXangle);Serial.print(",");
+  Serial.print(compAngleX);Serial.print(",");
+  Serial.print(kalAngleX);Serial.print(",");
+  
+  Serial.print(acc2Xangle);Serial.print(",");
+  Serial.print(gyro2Xangle);Serial.print(",");
+  Serial.print(compAngle2X);Serial.print(",");
+  Serial.print(kalAngle2X);Serial.println("");
+  
+//  Serial.print("\t");
+  /*
+  Serial.print(accYangle);Serial.print(",");
+  Serial.print(gyroYangle);Serial.print(",");
+  Serial.print(compAngleY); Serial.print(",");
+  Serial.print(kalAngleY);Serial.println("\t");
+  */
+  //Serial.print(temp);Serial.print("\t");
+   
+  //Serial.print("\r\n");
+  //delay(1);
+}
---
+++ ARDUINO/segway0.53/segway0.53.ino
@@ -0,0 +1,662 @@
+#define NUMREADINGS 5
+#include <FreeSixIMU.h>
+#include <FIMU_ADXL345.h>
+#include <FIMU_ITG3200.h>
+
+#include <Wire.h>
+
+
+
+const int AvgAngles = 3;
+ float prevTargetAngle = 0;
+ float targetAngle = 0;
+
+
+float angles[5];
+
+float currAngle, prevAngle;
+float prevAngles[AvgAngles];
+int prevAngleI = 0;
+
+// time vars
+int currTime = 0; 
+int prevTime = 0; 
+
+
+
+float errorSum = 0;
+float currError = 0;
+float prevError = 0;
+float iTerm = 0;
+float dTerm = 0;
+float pTerm = 0;
+
+//Location PID CONTROL - These are the PID control for the robot trying to hold its location.
+  float Lp = 0.5;
+  float Li = 0.05;
+  float Ld = 0.4;
+  float offsetLoc = 0;
+  float pT,iT,dT = 0;
+  float errorS = 0;
+  float prevE = 0;
+
+float imuValues[3];
+// Set the FreeSixIMU object
+FreeSixIMU sixDOF = FreeSixIMU();
+
+//FILter
+unsigned long loopTime = 0;
+const float COMPLEMENTARY_GAIN = 0.995;
+float lastPitch = 0;
+unsigned long lastStartTime = 0;
+float pitch = 0;
+int dt = 0;
+float gyro[3];
+float angle = 0;
+//**********************Setup Variables for the IR Sensor*********************
+//****************************************************************************
+int IRpin = 5;
+unsigned long IRDT = 0;
+int ledpin = 13;
+float distance = 0.00;
+//****************************************************************************
+//****************************************************************************
+
+
+//**********************Setup Variables for Gyro and Accel********************
+//****************************************************************************
+int readings[NUMREADINGS];                // the readings from the analog input
+int index = 0;                            // the index of the current reading
+int total = 0;                            // the running total
+double average = 0.00;                          // the average
+int rVal = 0;
+int refPin = 1;
+int rPin = 0;
+int yPin = 2;
+//*****************************************************************************
+//*****************************************************************************
+
+
+//********************Setup other Balancing Variables**************************
+//*****************************************************************************
+double vTorque = 0.0;
+double newTorque = 0.0;
+double newTor = 0.0;
+int z = 0;
+float a1 = 0.0;
+float a2 = 0.0;
+float a3 = 0.0;
+int m1Dir1 = 6;
+int m1Dir2 = 7;
+int m1PWM = 2;
+int m2Dir1 = 11;
+int m2Dir2 = 10;
+int m2PWM = 3;
+
+
+double vAngle = 0.0;
+double vHighPass = 0.0;
+double vLowPass = 0.0;
+double vSetpoint = 4.00;
+double vRate = 0.0;
+double last_angle = 0.0;
+double Pterm = 0.0;
+double Iterm = 0.0;
+double Istate = 0.0;
+double Dterm = 0.0;
+double Pgain = 1.9;
+double Igain = 0.12;
+double Dgain = 2.7;
+
+int mydt = 5;
+unsigned long last_PID = 0;
+unsigned long lastread = 0;
+double vYaxis = 0.00;
+
+//*****************************************************************************
+//*****************************************************************************
+
+//***********************Variables for driving*********************************
+//*****************************************************************************
+#define encoder0PinA  4
+#define encoder0PinB  5
+int vrDist = 0;
+int vlDist = 0;
+int last_vrDist = 0;
+int last_vlDist = 0;
+int vrencodeb = 0;
+double vrSpeed = 0;
+double last_vrSpeed = 0;
+int vlSpeed = 0;
+int vTurn = 0;
+int vTurnR = 0;
+int vTurnL = 0;
+int vDrive = 0;
+int vAdjust = 0;
+int vBalAdjust = 0;
+int vrDirection = 0;
+int last_vrDirection = 0;
+int myBaldt = 50;
+unsigned long lastBalupdate = 0;
+double sPterm = 0.0;
+double sIterm = 0.0;
+double sIstate = 0.0;
+double sDterm = 0.0;
+double sPgain = 3.5;
+double sIgain = 1.0;
+double sDgain = 4.5;
+double vrTarget_Speed = 0;
+
+
+void setup()
+{
+  
+  Serial.begin(9600);
+  Wire.begin();
+  
+  delay(5);
+  sixDOF.init(); //begin the IMU
+  delay(5);
+  Serial.begin(9600);                     // initialize serial communication with computer
+  
+  //Setup Motor pins*******************  
+  pinMode(m1Dir1,OUTPUT);
+  pinMode(m1Dir2,OUTPUT);
+  pinMode(m2Dir1,OUTPUT);
+  pinMode(m2Dir2,OUTPUT);
+  pinMode(m1PWM,OUTPUT);
+  pinMode(m2PWM,OUTPUT);
+  //***********************************
+  
+  //*Calibrate balance point************
+  delay(100);
+  /*
+  for (int a = 0; a < 20; a ++)
+  {
+  readings[a] = analogRead(yPin); // read from the sensor
+  if (a != 5)
+  total += readings[a];               // add the reading to the total
+  delay(40);
+  Serial.println(readings[a]);
+  }
+  */
+  
+  pinMode(encoder0PinA, INPUT); 
+  pinMode(encoder0PinB, INPUT); 
+  digitalWrite(encoder0PinA, HIGH);       // turn on pullup resistor
+  digitalWrite(encoder0PinB, HIGH);
+  attachInterrupt(1, rencoder, FALLING);
+
+ //***********************************
+ 
+}
+
+void loop()
+{
+  long starttime = millis();
+  //sixDOF.getEuler(angles);
+  void readAccel(int* x, int* y, int* z);
+  sixDOF.getValues(imuValues);  
+  //updateAngle();
+
+ 
+  //Serial.print(angles[0]);
+  //Serial.print(" | ");  
+  //Serial.print(angles[1]);
+  //Serial.print(" | ");
+  //Serial.println(angles[2]);
+
+
+  //**********************Main Code loop*******************************************
+  //*******************************************************************************
+  //*******************************************************************************
+
+  Complimentaryfilter();            //run complimentary filter function
+
+  //VTOR();
+  
+  Calculate_Torque();        //run torque calculations through PID
+  
+  
+  //Calculate_Speed();
+  
+  debugImu();
+  //Read_IR();
+
+  dt = millis()-starttime;
+
+  //*****************End Main Code Loop********************************************
+  //*******************************************************************************
+  //*******************************************************************************
+}
+
+
+//******************************************************************************* 
+//******************************************************************************* 
+//********************Setup PID loop to determine torque*************************
+
+void Calculate_Torque()
+{
+  if((millis()-last_PID) >= mydt)          // sample every dt ms -> 1000/dt hz.
+  { 
+    last_PID = millis();
+
+
+    Pterm = vAngle * Pgain;
+    Istate = (Istate + vAngle);
+    Iterm = Istate * Igain;
+    Dterm = (vAngle - last_angle) * Dgain;
+
+    a2 = angles[1];
+    a3 = angles[3];
+
+    Serial.print(" | ");
+    float aa = 3;
+    float ba = 2;
+    float ca = 4;
+if (angle > 0)
+      {
+       angle += 0.2;
+        }
+    if (angle < 0)
+      {
+       angle -= 0.2;
+        }   
+    
+    //newTorque = angle*19;
+    
+  if (angle > 30)
+  {
+    angle = 0.0;
+    vTorque = 0.0;
+    newTorque = 0.0;
+  }
+  if (angle < -30)
+  {
+    angle = 0.0;
+    vTorque = 0.0;
+    newTor = 0.0;
+  }
+ 
+    if (angle > 0)
+      {
+        vTorque = angle * 12 + 0.7 * newTorque;
+        //newTorque = vTorque;
+        }
+    if (angle < 0)
+      {
+        vTorque = angle * 12 + 0.7 * newTorque;
+        //newTor = vTorque;
+        }
+
+    if (angle < -1)
+      {
+        vTorque = angle * 6 + 0.7 * newTorque;
+        //newTor = vTorque;
+      }
+    if (angle > 1)
+      {
+        vTorque = angle * 6 + 0.7 * newTorque;
+        //newTorque = vTorque;
+      }
+
+    if (angle < -3)
+      {
+        vTorque = angle * 11 + 0.7 * newTorque;
+        //newTor = vTorque;
+      }
+    if (angle > 3)
+      {
+        vTorque = angle *11 + 0.7 * newTorque;
+        //newTorque = vTorque;
+      }
+
+      if (angle < -5)
+      {
+        vTorque = angle * 12 + 0.7 * newTorque;
+      //  newTor = vTorque;
+      }
+    if (angle > 5)
+      {
+        vTorque = angle * 12 + 0.7 * newTorque;
+      //  newTorque = vTorque;
+      }
+    if (angle < -10)
+      {
+        vTorque = angle * 14 + 0.7 * newTorque;
+    //    newTor = vTorque;
+      }
+    if (angle > 10)
+      {
+        vTorque = angle * 14 + 0.7 * newTorque;
+      //  newTorque = vTorque;
+      }
+        
+    if (angle = 0)
+     {
+      vTorque = 0;  
+     }
+
+  vTorque = vTorque;
+  
+    //vTorque = newTorque;
+
+       // vTorque = angle*20;//pow(fabs(angle), 1.0/3.0)*10 ;
+        
+  /*  if (angle<0){
+      vTorque = -vTorque;
+    }
+    */
+    last_angle = vAngle;
+
+    vTorque = constrain(vTorque,-255,255);
+    
+    Drive_Motor(vTorque);     //Send torque to motor drive code 
+  }
+  //*************End Setup PID Loop************************************************
+  //*******************************************************************************
+  //*******************************************************************************
+}
+
+//*********************Drive Motors**********************************************
+//*******************************************************************************
+//*******************************************************************************
+
+int Drive_Motor(int vTorque)
+{
+  if (vTorque >= 0)                                  //indicates drive motors forward
+  {
+   if (vTurn >= 0)                                 //indicates turn to the right
+     {
+      vTurnR = vTorque + vTurn;
+      vTurnL = vTorque - vTurn;
+      digitalWrite(m1Dir1, LOW);                       //Signals H-Bridge to move power forward
+      digitalWrite(m1Dir2, HIGH);
+      
+      if (vTurnL < 0)
+        {
+    digitalWrite(m2Dir1, HIGH);                       //Signals H-Bridge to move power forward
+        digitalWrite(m2Dir2, LOW);
+        }
+      else
+        {
+        digitalWrite(m2Dir1, LOW);                       //Signals H-Bridge to move power forward
+        digitalWrite(m2Dir2, HIGH);
+        }
+      vTurnL = abs(vTurnL);
+      vTurnR = abs(vTurnR);
+      analogWrite(m1PWM,vTurnR);
+      analogWrite(m2PWM,vTurnL);
+     }
+    
+   if (vTurn < 0)                                 //indicates turn to the left
+     {
+      vTurnR = vTorque + vTurn;
+      vTurnL = vTorque - vTurn;
+      digitalWrite(m1Dir1, LOW);                       //Signals H-Bridge to move power forward
+      digitalWrite(m1Dir2, HIGH);
+      
+      if (vTurnL < 0)
+        {
+    digitalWrite(m2Dir1, HIGH);                       //Signals H-Bridge to move power forward
+        digitalWrite(m2Dir2, LOW);
+        }
+      else
+        {
+        digitalWrite(m2Dir1, LOW);                       //Signals H-Bridge to move power forward
+        digitalWrite(m2Dir2, HIGH);
+        }
+      vTurnR = abs(vTurnR);
+      vTurnL = abs(vTurnL);
+      analogWrite(m1PWM,vTurnR);
+      analogWrite(m2PWM,vTurnL);
+     }
+  }
+
+else if (vTorque < 0)
+  {
+   vTorque = abs(vTorque);
+   if (vTurn >= 0)                                 //indicates turn to the right
+     {
+      vTurnR = vTorque - vTurn;
+      vTurnL = vTorque + vTurn;
+      digitalWrite(m2Dir1, HIGH);                       //Signals H-Bridge to move power forward
+      digitalWrite(m2Dir2, LOW);
+      
+      if (vTurnR < 0)
+        {
+    digitalWrite(m1Dir1, LOW);                       //Signals H-Bridge to move power forward
+        digitalWrite(m1Dir2, HIGH);
+        }
+      else
+        {
+        digitalWrite(m1Dir1, HIGH);                       //Signals H-Bridge to move power forward
+        digitalWrite(m1Dir2, LOW);
+        }
+      vTurnL = abs(vTurnL);
+      vTurnR = abs(vTurnR);
+      analogWrite(m1PWM,vTurnR);
+      analogWrite(m2PWM,vTurnL);
+     }
+    
+   if (vTurn < 0)                                 //indicates turn to the left
+     {
+      vTurnR = vTorque - vTurn;
+      vTurnL = vTorque + vTurn;
+      digitalWrite(m2Dir1, HIGH);                       //Signals H-Bridge to move power forward
+      digitalWrite(m2Dir2, LOW);
+      
+      if (vTurnR < 0)
+        {
+    digitalWrite(m1Dir1, LOW);                       //Signals H-Bridge to move power forward
+        digitalWrite(m1Dir2, HIGH);
+        }
+      else
+        {
+        digitalWrite(m1Dir1, HIGH);                       //Signals H-Bridge to move power forward
+        digitalWrite(m1Dir2, LOW);
+        }
+      vTurnR = abs(vTurnR);
+      vTurnL = abs(vTurnL);
+      analogWrite(m1PWM,vTurnR);
+      analogWrite(m2PWM,vTurnL);
+     }  
+
+  }
+  
+  
+
+
+  //******** End of Motor Drive Function*******************************************
+  //*******************************************************************************
+  //*******************************************************************************
+}
+
+
+
+
+//**************************Complimsentary Filtering*************************************
+//*******************************************************************************
+
+
+void Complimentaryfilter()
+{
+  updateAngle();
+  
+  if((millis()) )          // sample every dt ms -> 1000/dt hz.
+  { 
+    lastread = millis();
+    
+
+    sixDOF.getEuler(gyro);
+  // = 0.98 * (imuValues[0] + imuValues[4] * 10) + (0.02) * (imuValues[0]);  
+  angle = 0.03 * (angle + gyro[1] * dt/1000) + 0.97  * (angles[1]);
+  //angle = 0.99 * (angle + gyro[1] * dt) + 0.01 * (angles[1]);
+
+  //angle = 0.98 * gyro[1] + 0.02* angles[1];
+
+      Serial.print(newTorque);
+ 
+  }
+      if (angle > 0)
+    {
+      newTorque = newTorque + 25.0;
+    }
+    if (angle < 0)
+    {
+      newTorque = newTorque - 25.0;
+    }
+    if (angle == 0)
+    {
+      newTorque = 0.0;
+    }
+  //***********************************************************************
+  //*******************End Filtering********************************
+}
+
+//********************Calculate encoder************************************
+//*************************************************************************
+
+void rencoder()
+{
+  if (digitalRead(encoder0PinB) == HIGH) vrDist ++;
+  if (digitalRead(encoder0PinB) == LOW) vrDist --;
+}
+
+//********************Calculate speed**************************************
+//*************************************************************************
+
+void Calculate_Speed()
+{
+   if((millis()-lastBalupdate) >= myBaldt)  
+  { 
+    lastBalupdate = millis();
+    
+    vrSpeed = vrDist - last_vrDist;
+    vrSpeed = (vrSpeed / myBaldt);
+    vrSpeed = vrSpeed * 15.5;
+    
+    last_vrDist = 0;
+    vrDist = 0;
+    
+    sPterm = vrSpeed * sPgain;
+    sIstate = (sIstate + vrSpeed);
+    sIterm = sIstate * sIgain;
+    sDterm = (vrSpeed - last_vrSpeed) * sDgain;
+    
+    last_vrSpeed = vrSpeed;
+    
+    vrSpeed = 0.0;
+    
+    vrSpeed = sPterm + sDterm; 
+    
+    vrSpeed = map(vrSpeed,-400,400,-30,30);
+   
+    if (distance < 50.0){
+      vrTarget_Speed = 5;
+      digitalWrite(ledpin, HIGH);
+    }
+    else{
+      vrTarget_Speed = 0;
+      digitalWrite(ledpin, LOW);
+    }
+    
+    vrSpeed = vrSpeed + vrTarget_Speed;
+    //Serial.println(vrSpeed);
+   
+    vAngle = vAngle - vrSpeed;
+  }
+}
+
+//********************Read IR**********************************************
+//*************************************************************************
+
+void Read_IR()
+{
+  if((millis()-IRDT) >= 100)  
+  { 
+    IRDT = millis();
+    float volts = analogRead(IRpin)*0.0048828125;   // value from sensor * (5/1024) - if running 3.3.volts then change 5 to 3.3
+    distance = 65*pow(volts, -1.10);
+    //Serial.println(distance);
+  }
+}
+
+ 
+ void debugImu() {
+    sixDOF.getValues(imuValues);  
+
+ Serial.print("aX: ");
+ printInt(imuValues[0], 4);
+ Serial.print("\taY: ");
+ printInt(imuValues[1], 4);
+ Serial.print("\taZ: ");
+ printInt(imuValues[2], 4);
+ Serial.print("\tgX: ");
+ printInt(imuValues[3], 4);
+ Serial.print("\tgY: ");
+ printInt(imuValues[4], 4);
+ Serial.print("\tgZ: ");
+ printInt(imuValues[5], 4);
+  Serial.print("\n");
+ }
+ void printInt(int number, byte width) {
+ int currentMax = 10;
+ if (number < 0) 
+ currentMax = 1;
+ for (byte i=1; i<width; i++){
+ if (fabs(number) < currentMax) {
+ Serial.print(" ");
+ }
+ currentMax *= 10;
+ }
+ Serial.print(number);
+ }
+ void printFloat(float number, byte width) {
+ int currentMax = 10;
+ if (number < 0) 
+ currentMax = 1;
+ for (byte i=1; i<width; i++){
+ if (fabs(number) < currentMax) {
+ Serial.print(" ");
+ }
+ currentMax *= 10;
+ }
+ Serial.print(number);
+ }
+   
+void updateAngle() {
+  sixDOF.getYawPitchRoll(angles);
+  prevAngles[prevAngleI] = angles[1];
+  prevAngleI = (prevAngleI + 1) % AvgAngles;
+  float sum = 0;
+  for (int i = 0; i < AvgAngles; i++)
+      sum += prevAngles[i];
+  currAngle = sum / AvgAngles;
+  prevAngle = currAngle;
+  
+}
+void keyPressed() 
+{
+  vTorque = 0;
+  newTorque = 0;
+  newTor = 0;
+  angle = 0;
+}
+void VTOR()
+{
+  
+    if (angle > 0)
+    {
+      newTorque += 5.0;
+    }
+    if (angle < 0)
+    {
+      newTorque -= 5.0;
+    }
+    if (angle = 0)
+    {
+      newTorque = 0.0;
+    }
+}
+
